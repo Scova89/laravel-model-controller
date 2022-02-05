e902de77ddc2c7f995580a8b6939838eb825499f
Francesco <scovascova@gmail.com>
2022-02-05 17:14:20 +0100

    classe Movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
+        $movies = Movie::all();
+        dd($movies);
         return view("home");
     }
 }
